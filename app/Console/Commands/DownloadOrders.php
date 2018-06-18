6304dddcf1902d32620640e8b3acf63ecc2395b9
Jaime <jaimeucm@gmail.com>
2018-06-18 17:12:26 -0500

    Se agregan campos en DownloadOrders

--- app/Console/Commands/DownloadOrders.php
+++ app/Console/Commands/DownloadOrders.php
@@ -32,13 +32,31 @@ class DownloadOrders extends Command
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Comando para descargar lista de pedidos de clientes de Marco Polo';
 
     protected $calc;
     protected $detail;
     protected $order;
     protected $cat;
     protected $cli;
+
+    const SQL = "SELECT
+                    DOCNUM,
+                    CARDCODE,
+                    U_VIGENCIAINI,
+                    U_VIGENCIAFIN,
+                    DOCSTATUS,
+                    ITEMCODE,
+                    QUANTITY,
+                    U_CANTREQ,
+                    Confirmed,
+                    ORDR.ShipToCode,
+	                ORDR.Address2
+                FROM ORDR
+                LEFT JOIN RDR1
+                    ON ORDR.DocEntry = RDR1.DocEntry
+                WHERE ORDR.DocStatus = 'O'
+                    AND ORDR.Confirmed = 'Y'";
 
     /**
      * Create a new command instance.
@@ -49,11 +67,11 @@ class DownloadOrders extends Command
                                 CatalogoRepository $cata, ClienteRepository $clie)
     {
         parent::__construct();
-        $this->calc = $cal;
+        $this->calc   = $cal;
         $this->detail = $ordDet;
-        $this->order = $ord;
-        $this->cat = $cata;
-        $this->cli = $clie;
+        $this->order  = $ord;
+        $this->cat    = $cata;
+        $this->cli    = $clie;
     }
 
     /**
@@ -64,169 +82,139 @@ class DownloadOrders extends Command
     public function handle()
     {
         //Sericio del bajado de pedidos
+        //---------------------------------------------------------------------------------------
+        $servername = '192.168.71.4';
+        $connectionInfo = array(
+            'Database'             =>  'MARCOPOLO_PROD' ,
+            'UID'                  => 'sim',
+            'PWD'                  => '~Cs_z~Ww',
+            'ReturnDatesAsStrings' => true,
+            'CharacterSet'         => 'UTF-8'
+        );
 
-            //---------------------------------------------------------------------------------------
+        $con = sqlsrv_connect($servername, $connectionInfo);
 
-
-
-            $servername = '192.168.71.4';
-            $connectionInfo = array('Database' =>  'MARCOPOLO_PROD' , 'UID' => 'sim',
-                                'PWD' => '~Cs_z~Ww','ReturnDatesAsStrings'=>true, 'CharacterSet' => 'UTF-8');
-
-            $con = sqlsrv_connect($servername,$connectionInfo);
-
-            if($con){
-
-                $sql = "select DOCNUM,CARDCODE,U_VIGENCIAINI,U_VIGENCIAFIN,DOCSTATUS,ITEMCODE,QUANTITY,U_CANTREQ,Confirmed from ORDR".
-                       " left join RDR1 ON ORDR.DocEntry = RDR1.DocEntry where ORDR.DocStatus = 'O' and ORDR.Confirmed = 'Y'";
-            $stmt = sqlsrv_query( $con, $sql );
-
-            /*while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
-
-                Log::info(" pedido:  ".$row['DOCNUM']." , ".$row['CARDCODE']." , ".$row['U_VIGENCIAINI'].
-                          " , ".$row['U_VIGENCIAFIN']." , ".$row['DOCSTATUS']." , ".$row['Confirmed']." , ".$row['ITEMCODE'].
-                          " , ".$row['QUANTITY']." , ".$row['U_CANTREQ']);
-            }*/
+        if($con){
+            $stmt = sqlsrv_query( $con, self::SQL);
 
             while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
-
-                
-
-                if($row['CARDCODE'] == "CLN-0044-0001"){
-
+                Log::info("DownloadOrders - item: [".implode(" , ", $row)."]");
+                if($row['CARDCODE'] == "CLN-0044-0001") {
                     Log::info(" pedido:  ".$row['DOCNUM']." , ".$row['CARDCODE']." , ".$row['U_VIGENCIAINI'].
-                          " , ".$row['U_VIGENCIAFIN']." , ".$row['DOCSTATUS']." , ".$row['Confirmed']." , ".$row['ITEMCODE'].
-                          " , ".$row['QUANTITY']." , ".$row['U_CANTREQ']);
+                            " , ".$row['U_VIGENCIAFIN']." , ".$row['DOCSTATUS']." , ".$row['Confirmed']." , ".$row['ITEMCODE'].
+                            " , ".$row['QUANTITY']." , ".$row['U_CANTREQ']);
 
-                $fechaI = strtotime($row['U_VIGENCIAINI']);
-                $fechaF = strtotime($row['U_VIGENCIAFIN']);
+                    $fechaI = strtotime($row['U_VIGENCIAINI']);
+                    $fechaF = strtotime($row['U_VIGENCIAFIN']);
 
-                $data1 = array(
-                    OrderRepository::SQL_CODIGO_ORDEN  => $row['DOCNUM'],
-                    OrderRepository::SQL_INICIO  => date('Y-m-d', $fechaI),
-                    OrderRepository::SQL_FIN => date('Y-m-d', $fechaF),
-                    OrderRepository::SQL_ESTATUS => 1,
-                    OrderRepository::SQL_CODIGO   => $row['CARDCODE']
-                );
+                    $data1 = array(
+                        OrderRepository::SQL_CODIGO_ORDEN   => $row['DOCNUM'],
+                        OrderRepository::SQL_INICIO         => date('Y-m-d', $fechaI),
+                        OrderRepository::SQL_FIN            => date('Y-m-d', $fechaF),
+                        OrderRepository::SQL_ESTATUS        => 1,
+                        OrderRepository::SQL_CODIGO         => $row['CARDCODE']
+                    );
+                    //función para restar fechas, regresa los dias restantes
 
-                
+                    //------------------------------------------------------
+                    $datetime1 = new DateTime(date('Y-m-d', $fechaI));
+                    $datetime2 = new DateTime(date('Y-m-d', $fechaF));
+                    $interval = $datetime1->diff($datetime2);
+                    //DV
+                    $daysValidity = intval($interval->format("%d"));
 
-                //función para restar fechas, regresa los dias restantes
+                    //------------------------------------------------------
+                    $datetime3 = new DateTime("now");
+                    $interval1 = $datetime1->diff($datetime3);
+                    //DF
+                    $daysLeft = intval($interval1->format("%d"));
 
-                //------------------------------------------------------
+                    $c1 = $this->cat->getByLabel('c1');
+                    $c2 = $this->cat->getByLabel('c2');
+                    $c3 = $this->cat->getByLabel('c3');
+                    $c6 = $this->cat->getByLabel('c6');
 
-                $datetime1 = new DateTime(date('Y-m-d', $fechaI));
-                $datetime2 = new DateTime(date('Y-m-d', $fechaF));
-                $interval = $datetime1->diff($datetime2);
-                //DV
-                $daysValidity = intval($interval->format("%d"));
+                    $validity = 0;
 
-                //------------------------------------------------------
+                    if($daysValidity > $c1) {
 
-                $datetime3 = new DateTime("now");
-                $interval1 = $datetime1->diff($datetime3);
-                //DF
-                $daysLeft = intval($interval1->format("%d"));
+                        $validity = (($daysValidity/$c2)+$daysLeft+$c3);
 
-                $c1 = $this->cat->getByLabel('c1');
-                $c2 = $this->cat->getByLabel('c2');
-                $c3 = $this->cat->getByLabel('c3');
-                $c6 = $this->cat->getByLabel('c6');
+                    } else if($daysValidity < $c1) {
 
-                $validity = 0;
+                        $validity = (($daysValidity/$c2)+$daysLeft);
 
-                if($daysValidity > $c1){
-
-                    $validity = (($daysValidity/$c2)+$daysLeft+$c3);
-
-                }else if($daysValidity < $c1){
-
-                    $validity = (($daysValidity/$c2)+$daysLeft);
-
-                }
-
-                $V = 0;
-
-                if($validity == 1){
-
-                    $V = 90;
-
-                }else if($validity == 2){
-
-                    $V = 50;
-
-                }else if($validity > 3){
-
-                    $V = (50 - (2*($validity - 2)));
-
-                    if($V < 0){
-
-                        $V = 0;
                     }
 
-                }
+                    $V = 0;
 
-                $fechaHoy = date('Y-m-j');
+                    if($validity == 1){
 
-                $fp = strtotime ( '+'.$validity.' day' , strtotime ( $fechaHoy ) );
+                        $V = 90;
 
-                $fp = date('Y-m-d', $fp);
+                    } else if($validity == 2){
 
-                $cliente = $this->cli->getByCodigo($row['CARDCODE']);
+                        $V = 50;
 
-                //$tamanioPedido = ?;
+                    } else if($validity > 3){
 
-                $D = floor(($cliente->CE * $cliente->TP) / $c6);
+                        $V = (50 - (2*($validity - 2)));
 
+                        if($V < 0){
+                            $V = 0;
+                        }
 
-                //dist ???
+                    }
 
-                $dist = 10;
+                    $fechaHoy = date('Y-m-j');
 
-                $priority = floor((($cliente->P + $V + $D) / 2) + $dist);                            
+                    $fp = strtotime ( '+'.$validity.' day' , strtotime ( $fechaHoy ) );
 
-                if($this->order->getByCode($row['DOCNUM']) == null){
+                    $fp = date('Y-m-d', $fp);
 
-                    $this->order->create($data1);
+                    $cliente = $this->cli->getByCodigo($row['CARDCODE']);
 
-                    $idOrderEsp = $this->order->getByCode($row['DOCNUM'])->id;
+                    //$tamanioPedido = ?;
 
-                    $data3 = array(
-                        CalculationRepository::SQL_P => $cliente->P,
-                        CalculationRepository::SQL_V => $V,
-                        CalculationRepository::SQL_D => $D,
-                        CalculationRepository::SQL_PRIORITY => $priority,
-                        CalculationRepository::SQL_DIST => 12,
-                        CalculationRepository::SQL_FP => $fp,
-                        CalculationRepository::SQL_ORDID => $idOrderEsp
+                    $D = floor(($cliente->CE * $cliente->TP) / $c6);
+
+                    //dist ???
+
+                    $dist = 10;
+
+                    $priority = floor((($cliente->P + $V + $D) / 2) + $dist);
+
+                    if($this->order->getByCode($row['DOCNUM']) == null){
+
+                        $this->order->create($data1);
+
+                        $idOrderEsp = $this->order->getByCode($row['DOCNUM'])->id;
+
+                        $data3 = array(
+                            CalculationRepository::SQL_P        => $cliente->P,
+                            CalculationRepository::SQL_V        => $V,
+                            CalculationRepository::SQL_D        => $D,
+                            CalculationRepository::SQL_PRIORITY => $priority,
+                            CalculationRepository::SQL_DIST     => 12,
+                            CalculationRepository::SQL_FP       => $fp,
+                            CalculationRepository::SQL_ORDID    => $idOrderEsp
+                        );
+                        $this->calc->create($data3);
+                    }
+
+                    $idOrder = $this->order->getByCode($row['DOCNUM'])->id;
+                    $data2 = array(
+                        OrderDetailRepository::SQL_CODIGOP   => $row['ITEMCODE'],
+                        OrderDetailRepository::SQL_CANTIDAD  => $row['QUANTITY'],
+                        OrderDetailRepository::SQL_CANTIDADP => $row['U_CANTREQ'],
+                        OrderDetailRepository::SQL_ORDEN_ID  => $idOrder
                     );
 
-                    $this->calc->create($data3);
-                    
+                    $this->detail->create($data2);
                 }
-
-                $idOrder = $this->order->getByCode($row['DOCNUM'])->id;
-
-                $data2 = array(
-                    OrderDetailRepository::SQL_CODIGOP => $row['ITEMCODE'],
-                    OrderDetailRepository::SQL_CANTIDAD => $row['QUANTITY'],
-                    OrderDetailRepository::SQL_CANTIDADP => $row['U_CANTREQ'],
-                    OrderDetailRepository::SQL_ORDEN_ID => $idOrder
-                );
-
-                $this->detail->create($data2);
-
             }
-
+        } else {
+            Log::error(" DownloadOrders - error:  ".implode(" \n ", sqlsrv_errors()));
         }
-
-
-
-
-            }else{
-
-                Log::info(" DownloadOrders - error:  ".sqlsrv_errors());
-
-            }
     }
 }
