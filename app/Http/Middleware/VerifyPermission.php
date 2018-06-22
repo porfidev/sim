c7607c6a5e240ad16d7c03335719d4d66c846889
Jaime <jaimeucm@gmail.com>
2018-06-22 18:25:18 -0500

    Se modifican permisos y se crea script para cargar tablas de menu y profiles

--- app/Http/Middleware/VerifyPermission.php
+++ app/Http/Middleware/VerifyPermission.php
@@ -41,7 +41,7 @@ class VerifyPermission
             $rol = $user->rol;
             $permission = $rol
                 ->getMenuItems()
-                ->where(MenuRepository::SQL_URL, 'like', $url)
+                ->where(MenuRepository::SQL_URL, 'like', $url."%")
                 ->first();
             Log::debug("VerifyPermission - permission: ".json_encode($permission));
             if(empty($permission)) {
